0ea2416afb553aea971be34ccb75606f2b6453d8
Mrs Qu <862792055@qq.com>
2019-10-29 20:56:50 +0800

    细节

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,6 @@
-// pages/home/home.js
+import {
+  request
+} from '../../utils/request.js';
 Page({
 
   /**
@@ -11,11 +13,24 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
-  bindPickerChange: function (e) {
+  // 选择经销商
+  getDealer() {
+    request({
+      url: 'http://www.icprj.com/IC/api/faPublic/list',
+      data: {
+        type: 1
+      }
+    }).then(res => {
+
+    });
+  },
+
+
+  bindPickerChange: function(e) {
     this.setData({
       index: e.detail.value
     })
@@ -24,49 +39,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,66 +1,127 @@
-// pages/login/login.js
+import {
+  request
+} from '../../utils/request.js';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    tel: '18768871893',
+    passworld: '123456'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
+  login() {
+    let {
+      tel,
+      passworld
+    } = this.data;
+    if (!tel.trim()) {
+      wx.showToast({
+        title: '请输入手机号',
+        icon: 'none'
+      });
+      return
+    } else if (!/^[1][3,4,5,7,8][0-9]{9}$/.test(tel)) {
+      wx.showToast({
+        title: '请输入正确的手机号',
+        icon: 'none'
+      });
+      return
+    } else if (!passworld.trim()) {
+      wx.showToast({
+        title: '请输入密码',
+        icon: 'none'
+      });
+      return
+    } else if (passworld.length < 6) {
+      wx.showToast({
+        title: '密码不能少于6位',
+        icon: 'none'
+      });
+      return
+    }
+    wx.showLoading({
+      title: '登录中...',
+    })
+    request({
+      url: 'http://www.icprj.com/IC/api/faPublic/list',
+      data: {
+        type: 1
+        // tel: tel,
+        // passworld: passworld
+      }
+    }).then(res => {
+      wx.hideLoading();
+      wx.switchTab({
+        url: '/pages/home/home'
+      });
+    });
+  },
 
+
+  bindTel(e) {
+    this.setData({
+      tel: e.detail.value
+    })
+  },
+  bindPassworld(e) {
+    this.setData({
+      passworld: e.detail.value
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
