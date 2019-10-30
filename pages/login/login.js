5c234f85fcb9d69d4e71c43caaa98c3620dfdf1f
Mrs Qu <862792055@qq.com>
2019-10-30 09:50:29 +0800

    页面

--- pages/login/login.js
+++ pages/login/login.js
@@ -18,51 +18,57 @@ Page({
 
   },
   login() {
-    let {
-      tel,
-      passworld
-    } = this.data;
-    if (!tel.trim()) {
-      wx.showToast({
-        title: '请输入手机号',
-        icon: 'none'
-      });
-      return
-    } else if (!/^[1][3,4,5,7,8][0-9]{9}$/.test(tel)) {
-      wx.showToast({
-        title: '请输入正确的手机号',
-        icon: 'none'
-      });
-      return
-    } else if (!passworld.trim()) {
-      wx.showToast({
-        title: '请输入密码',
-        icon: 'none'
-      });
-      return
-    } else if (passworld.length < 6) {
-      wx.showToast({
-        title: '密码不能少于6位',
-        icon: 'none'
-      });
-      return
-    }
     wx.showLoading({
       title: '登录中...',
     })
-    request({
-      url: 'http://www.icprj.com/IC/api/faPublic/list',
-      data: {
-        type: 1
-        // tel: tel,
-        // passworld: passworld
-      }
-    }).then(res => {
-      wx.hideLoading();
-      wx.switchTab({
-        url: '/pages/home/home'
-      });
+    wx.switchTab({
+      url: '/pages/home/home'
     });
+    // let {
+    //   tel,
+    //   passworld
+    // } = this.data;
+    // if (!tel.trim()) {
+    //   wx.showToast({
+    //     title: '请输入手机号',
+    //     icon: 'none'
+    //   });
+    //   return
+    // } else if (!/^[1][3,4,5,7,8][0-9]{9}$/.test(tel)) {
+    //   wx.showToast({
+    //     title: '请输入正确的手机号',
+    //     icon: 'none'
+    //   });
+    //   return
+    // } else if (!passworld.trim()) {
+    //   wx.showToast({
+    //     title: '请输入密码',
+    //     icon: 'none'
+    //   });
+    //   return
+    // } else if (passworld.length < 6) {
+    //   wx.showToast({
+    //     title: '密码不能少于6位',
+    //     icon: 'none'
+    //   });
+    //   return
+    // }
+    // wx.showLoading({
+    //   title: '登录中...',
+    // })
+    // request({
+    //   url: 'http://www.icprj.com/IC/api/faPublic/list',
+    //   data: {
+    //     type: 1
+    //     // tel: tel,
+    //     // passworld: passworld
+    //   }
+    // }).then(res => {
+    //   wx.hideLoading();
+    //   wx.switchTab({
+    //     url: '/pages/home/home'
+    //   });
+    // });
   },
 
 
